4debbed9d31e5e52790cef9ab9d38d2c24016ea9
dev vats <vatsdev27@gmail.com>
2023-11-16 14:40:26 +0530

    add static page

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload again 1234.
+          Edit <code>src/App.js</code> and save to reload again 1234567.
         </p>
         <a
           className="App-link"
