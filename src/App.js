2dde08dcebc9090a6d327eae6a6e68597dfcbf2d
dev vats <vatsdev27@gmail.com>
2023-11-20 16:33:25 +0530

    add console log

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload again 12345676.
+          Edit <code>src/App.js</code> and save to reload again 1234567890.
         </p>
         <a
           className="App-link"
